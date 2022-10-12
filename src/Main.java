a7c16041fe7889a80bd052472853214b41ea5f61
Motoc <motococtavian71@gmail.com>
2022-10-12 13:08:05 +0300

    Final Version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+          int[] numbers = { 1,2,3 };
+        System.out.println(average(numbers).getAsDouble());
+    }
+
+    public static OptionalDouble average(int[] list) {
+        return Arrays.stream(list).average();
     }
 }
